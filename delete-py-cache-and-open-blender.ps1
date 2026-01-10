60ca0e7a50f4b4e730ad71be13ec691b6413279f
CattaRappa <sambow23@gmail.com>
2026-01-10 16:25:13 -0600

    main: remove full path from ps1 script whoops

--- delete-py-cache-and-open-blender.ps1
+++ delete-py-cache-and-open-blender.ps1
@@ -1,6 +1,6 @@
-Remove-Item -Path "C:\Users\cr\blender-remix\rtx_remix_importer\operators\__pycache__" -Recurse -Force
-Remove-Item -Path "C:\Users\cr\blender-remix\rtx_remix_importer\ui\operators\__pycache__" -Recurse -Force
-Remove-Item -Path "C:\Users\cr\blender-remix\rtx_remix_importer\ui\__pycache__" -Recurse -Force
-Remove-Item -Path "C:\Users\cr\blender-remix\rtx_remix_importer\__pycache__" -Recurse -Force
+Remove-Item -Path .\rtx_remix_importer\operators\__pycache__ -Recurse -Force
+Remove-Item -Path .\rtx_remix_importer\ui\operators\__pycache__ -Recurse -Force
+Remove-Item -Path .\rtx_remix_importer\ui\__pycache__ -Recurse -Force
+Remove-Item -Path .\rtx_remix_importer\__pycache__ -Recurse -Force
 
 blender
